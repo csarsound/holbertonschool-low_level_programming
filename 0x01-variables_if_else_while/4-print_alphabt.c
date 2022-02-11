9a24bc80d1930cb4f4ba74565dedd4b73fccf0a6
csarsound <csar_sound@hotmail.com>
2022-02-11 15:38:12 -0800

    AlphabetSoup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - designated start of the program
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101)
+		{
+		}
+		else if (i == 113)
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
