bff0b840073406d7c48cc567be2d4e7c3124e375
csarsound <csar_sound@hotmail.com>
2022-02-11 14:36:31 -0800

    ISometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - designated start of the program
+ * Return: Always 0
+ */
+int main(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++)
+		putchar(e);
+	putchar('\n');
+		return (0);
+
+}
