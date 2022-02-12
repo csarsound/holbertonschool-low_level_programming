07869f9c5c720294bedd7ed0e62d1afd7e71896f
csarsound <csar_sound@hotmail.com>
2022-02-11 17:41:24 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - designated start of the program
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int clock;
+
+	for (clock = 0; clock < 10; clock++)
+	printf("%d", clock);
+	putchar('\n');
+
+	return (0);
+}
