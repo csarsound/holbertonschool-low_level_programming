cfaf884de5493aeda6b62a0641ca5de258ba2496
csarsound <csar_sound@hotmail.com>
2022-02-11 19:10:47 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print al the numbers singles
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sin;
+
+	for (sin = '0'; sin <= 9; sin++)
+	{
+		putchar(sin + '0');
+	if (sin < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
