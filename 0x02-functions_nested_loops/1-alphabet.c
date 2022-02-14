0543aa30a3bc134967425ddb6b9ae53049fbbc59
csarsound <csar_sound@hotmail.com>
2022-02-14 14:07:30 -0800

    AlphabetGame

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char z;
+
+	for (z = 'a'; z <= 'z'; z++)
+		_putchar(z);
+	_putchar('\n');
+}
