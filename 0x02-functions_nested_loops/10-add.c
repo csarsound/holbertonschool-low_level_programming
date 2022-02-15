a843909df9057078e7b5bdee5d0573e9db746adb
csarsound <csar_sound@hotmail.com>
2022-02-15 14:40:23 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that add two integers
+ * @j: integer input
+ * @k: integer input
+ * Return: sum of two integers
+ */
+int add(int j, int k)
+{
+	return (j + k);
+}
+
