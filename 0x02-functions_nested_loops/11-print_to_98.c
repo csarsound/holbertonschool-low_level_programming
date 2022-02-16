2067c2f561944b26a12b79c71dd5d8938e5090bc
csarsound <csar_sound@hotmail.com>
2022-02-16 07:11:41 -0800

    BatteryStreet

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_to_98 - print all numbers almost 98
+ *@n: First number
+ *Return: 98 numbers
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+			printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
+
