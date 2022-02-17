2b99d12818f56038b75b41a755a87d241b4bb749
csarsound <csar_sound@hotmail.com>
2022-02-17 07:45:46 -0800

    MultipleMains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * positive_or_negative  - test function that prints if is positive or negative
- * @i: Description of a parameter empt
- * @positive_or_negative - come the problem
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
 
 	i = 0;
 	positive_or_negative(i);
 
+	return (0);
 }
-
