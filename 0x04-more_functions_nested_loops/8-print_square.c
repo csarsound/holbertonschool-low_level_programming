96e786956c1201378b87ab3e837070c6fa42a44d
csarsound <csar_sound@hotmail.com>
2022-02-17 15:40:06 -0800

    YouAreSoMuchSunshinein every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - Draws a square
+ * @size: Parameter of size
+ * Return: 0 value
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size == 0)
+		_putchar('\n');
+}
