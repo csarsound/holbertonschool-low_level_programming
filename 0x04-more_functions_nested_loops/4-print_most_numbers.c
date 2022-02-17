145837172cc7a9e18a52ef26440c1c8a63159aa2
csarsound <csar_sound@hotmail.com>
2022-02-17 13:10:19 -0800

    IBelieveInNumbersAndSigns

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function of numbers
+ * Return: numbers without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52) 
+			_putchar(n);
+	}
+	_putchar('\n');
+}
