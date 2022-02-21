606611e1433c39f0700420ca48c0ce8e8e1fdf68
csarsound <csar_sound@hotmail.com>
2022-02-21 14:31:13 -0800

    DontSwapHorses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap integers
+ * @a: first integers 98
+ * @b: second integers 42
+ * Return: swap_int
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	*a = 42;
+	*b = 98;
+}
