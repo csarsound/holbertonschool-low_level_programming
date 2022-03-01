e935b9d1fe5edfec1fc8f3d59d47a147a77acd34
csarsound <csar_sound@hotmail.com>
2022-02-28 19:02:58 -0800

    TheLineOfLifeIsARagged

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - sum of two diagonals
+ * @a: pointer
+ * @size: size of array
+ * Return: sum
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < (size * size); i += size + 1)
+	{
+		sum1 += a[i];
+	}
+	for (j = size - 1; j < (size * size) - 1; j += size - 1)
+	{
+		sum2 += a[j];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
