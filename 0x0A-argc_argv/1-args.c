fa5b5f983704920c2cd46dd848c1beed40d42372
csarsound <csar_sound@hotmail.com>
2022-03-04 12:26:48 -0800

    SilenceIsArgument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print argument
+ * @argc: count
+ * @argv: vector
+ * Return: zero
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
