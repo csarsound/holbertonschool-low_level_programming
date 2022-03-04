960bfeee446f2b1f2f7c073fe23f8d8d54374939
csarsound <csar_sound@hotmail.com>
2022-03-04 13:37:55 -0800

    ToInfinityAndBeyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add sum of numbers positive
+ * @argc: counter
+ * @argv: vector
+ * Return: Zero or One if are letters
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
