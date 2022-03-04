84182cc065e01b85e0f2fa9e939b5243fa929efd
csarsound <csar_sound@hotmail.com>
2022-03-04 12:10:15 -0800

    ItAin'tWhatTheyCallYou

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: counter
+ * @argv: vertice
+ * Return: Zero
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
