79362405c60ea6d052463c63aac4a5c047b6907a
csarsound <68489879+csarsound@users.noreply.github.com>
2022-03-30 18:39:24 -0500

    ListLenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *list_len - Return number of elements in list
+ *@h: Pointer to head
+ *Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+	const list_t *tmp;
+
+	tmp = h;
+
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
