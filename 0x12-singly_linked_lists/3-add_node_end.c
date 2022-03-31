3866bfb125a1f9c4f26b134108a02e62535b8911
csarsound <68489879+csarsound@users.noreply.github.com>
2022-03-30 19:51:46 -0500

    AddNoteAtTheEnd

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ *add_node_end - Function to add item at the end of a linked_list
+ *@head: Head pointer
+ *@str: String to add
+ *Return: address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *tmp;
+	unsigned int i = 0;
+
+	if (str == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (new_node->str[i])
+	{
+		i++;
+	}
+	new_node->len = i;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	tmp = *head;
+
+	for (; tmp->next; i++)
+
+	tmp = tmp->next;
+	tmp->next = new_node;
+
+	return (new_node);
+}
