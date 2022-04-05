f05cc38875bc81f159212ba2b3008673c5446deb
csarsound <68489879+csarsound@users.noreply.github.com>
2022-04-05 16:22:40 -0500

    TreadLightly

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX.
+ * @filename: Pointer to file
+ * @letters: Number of letters
+ * Return: Letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t n_bytes;
+
+	if (filename)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd < 0)
+			return (0);
+
+		buf = malloc(letters * sizeof(char));
+
+		if (buf == NULL)
+			return (0);
+
+		n_bytes = read(fd, buf, letters);
+
+		if (n_bytes < 0)
+			return (0);
+
+		n_bytes = write(STDOUT_FILENO, buf, n_bytes);
+
+		if (n_bytes < 0)
+			return (0);
+
+		close(fd);
+		free(buf);
+		return (n_bytes);
+	}
+	return (0);
+}
