b1632ce3463be91b8466436860de633da2402ffb
csarsound <csarsound@hotmail.com>
2022-08-11 22:11:41 -0500

    Fibonacci<50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,26 @@ int main(void)
 {
 	int index, num1 = 1, num2 = 2, sum;
 
+	if (num1 == 1 && num2 == 2)
+	{
 	printf("%d, ", num1);
 	printf("%d, ", num2);
+	}
 	for (index = 0; index <= 50; index++)
 	{
 		sum = (num1 + num2);
-		printf("%d, ", sum);
+		printf("%d", sum);
 		num1++;
 		num2++;
-		if (index == 50)
+		if (index == 49)
 		{
 			printf("\n");
 			return (0);
 		}
+		else
+		{
+			printf(", ");
+		}
+		
 	}
 }
