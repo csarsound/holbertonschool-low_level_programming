1af5983dc775d0f2c816f806530a9eed19e97d74
csarsound <csarsound@hotmail.com>
2022-08-20 14:49:42 -0500

    InComputerclass

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 
 	printf("%lu, ", sum1);
 
-	for (i = 1; i <= 49; i++)
+	for (i = 1; i < 49; i++)
 	{
 		sum1 = sum1 + sum2;
 		printf("%lu, ", sum1);
 		sum2 = sum1 + sum2;
-		if (i <= 49)
+		if (i < 49)
 		{
 			printf("%lu, ", sum2);
 		}
