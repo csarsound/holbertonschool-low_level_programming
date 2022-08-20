75dd288942f290066c66165d20371a75b2039c38
csarsound <csarsound@hotmail.com>
2022-08-20 14:47:53 -0500

    InComputerclass

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function that print the first 98 numbers fibonacci
+ * @void: nothing
+ * Return: Always o.
+ */
+int main(void)
+{
+	unsigned long int i, sum1 = 1, sum2 = 1;
+
+	printf("%lu, ", sum1);
+
+	for (i = 1; i <= 49; i++)
+	{
+		sum1 = sum1 + sum2;
+		printf("%lu, ", sum1);
+		sum2 = sum1 + sum2;
+		if (i <= 49)
+		{
+			printf("%lu, ", sum2);
+		}
+	}
+	printf("\n");
+	return (0);
+}
