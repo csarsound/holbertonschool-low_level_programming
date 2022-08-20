87ad99cf4d81dcc5f8b2ec25d0981e7c0b385e14
csarsound <csarsound@hotmail.com>
2022-08-20 14:19:13 -0500

    10XAlphabet

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+/**
+ * main - function that print the sum of all the pair numbers
+ * @void: nothing
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int sum1 = 1, sum2 = 2, sumt = 0, next;
+
+	for (i = 0; i <= 33; i++)
+	{
+		if (sum1 < 40000 && (sum1 % 2) == 0)
+		{
+			sumt = sumt + sum1;
+		}
+		next = sum1 + sum2;
+		sum1 = sum2;
+		sum2 = next;
+	}
+	printf("%lu\n", sumt);
+
+	return (0);
+}
