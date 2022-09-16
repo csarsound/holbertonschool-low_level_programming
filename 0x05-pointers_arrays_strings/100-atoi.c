aa1cfba385d4e673f07cd61acc0f0effc24a91e3
csarsound <csarsound@hotmail.com>
2022-09-16 18:05:45 -0500

    Gread Leaders

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - function that print just the integers
+ * @s: string
+ * Return: Nothing
+ **/
+int _atoi(char *s)
+{
+	int i = 0, integer = 0, menos = 1, end = 1;
+
+	for (i = 0; s[i] != '\0' && end; i++)
+	{
+		if (s[i] == '-')
+		{
+			menos *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			integer *= 10;
+			integer += s[i] - '0';
+		}
+		else if (integer > 0)
+		{
+			end = 0;
+		}
+	}
+	integer *= menos;
+	return (integer);
+}
