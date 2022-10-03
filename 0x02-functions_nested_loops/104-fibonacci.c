e59a473e5713588c216cb1454a1e124d3703c98b
csarsound <csarsound@hotmail.com>
2022-10-03 16:08:52 -0500

    Fibonachhi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	int i;
-	unsigned long int initial = 0, initial2 = 1, generade = 0;
+	int initial = 0, initial2 = 1, generade = 0;
 
-	for ( i = 1; i < 98; i++)
+	for ( i = 1; i <= 98; i++)
 	{
 	generade = initial + initial2;
 	initial = initial2;
 	initial2 = generade;
-	printf("%lu, ", generade);
+	printf("%d, ", generade);
 	}
 	return (0);
 }
